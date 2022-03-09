72d964987f781cdca298f0c4362675a7a53f3a71
Lorena Nascimento <lorenanascimento28@gmail.com>
2022-03-09 19:57:17 -0300

    Aula 6 - Estrutura de Dados: Computação na prática com Java

---
+++ estruturadedados/src/estruturadedados/fila/TesteDaFila.java
@@ -0,0 +1,29 @@
+package estruturadedados.fila;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TesteDaFila {
+
+	public static void main(String[] args) {
+		
+		Fila fila = new Fila();
+		
+		fila.adiciona("Mauricio");
+		fila.adiciona("Guilherme");
+
+		System.out.println(fila);
+		
+		String x1 = fila.remove();
+		System.out.println(x1);
+		System.out.println(fila);
+		
+		//implementacao do java para utilizacao de fila
+		Queue<String> filaDoJava = new LinkedList<String>();
+		filaDoJava.add("Mauricio");
+		String x2 = filaDoJava.poll();
+		System.out.println(x2);
+		System.out.println(filaDoJava);
+	}
+
+}
